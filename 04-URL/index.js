f1ced2437f86613aaa3a60b3dd4fe1d658832f0e
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-09-02 17:47:02 +0530

    url concepts learned

--- 04-URL/index.js
+++ 04-URL/index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+
 
 let ctr = 0;
 const myServer = http.createServer((req, res) => {
@@ -8,13 +10,19 @@ const myServer = http.createServer((req, res) => {
     
     // append incoming req to log file
     let log = `${Date.now()}: ${req.url} Hello user \n`
+    if (req.url === '/favicon.ico') return res.end();
+
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
     fs.appendFile('log.txt', log, (err, data) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/': 
                 res.end('Homepage');
                 break;
             case '/about':
-                res.end('Hello my name is avadhoot');
+                const userName = myUrl.query.myname;
+                res.end(`Hello, ${userName}`);
                 break;
             default:
                 res.end('404 not found');
