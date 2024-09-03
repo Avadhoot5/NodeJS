8552a99ac47b6ba11509cc87949fe4d193dbd1b7
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-09-03 12:52:23 +0530

    added post patch delete routes for users

--- 06-project01/index.js
+++ 06-project01/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const users = require('./MOCK_DATA.json');
+const fs = require('fs');
 
+app.use(express.json());
 
 // rendering HTML document for users route
 // SSR page
@@ -26,12 +28,54 @@ app.get('/api/users', (req, res) => {
 app.get('/api/users/:id', (req, res) => {
     const userId = req.params.id;
     let user = users.find((user) => user.id === parseInt(userId));
+    if (!user) {
+        res.status(400).json({message: "user not found"});
+    }
     res.json(user);
 })
 
 app.post('/api/users', (req, res) => {
-    console.log('User created');
-    return res.json({status: 'pending'});
+    const id = users.length + 1;
+    const userData = {id, ...req.body};
+    users.push(userData);
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        if (err) console.log("not able to write the data");
+        return res.status(201).json({'User created sucessfully!': id});
+    })
+})
+
+app.patch('/api/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === userId);
+    const updatedDetails = {id: userId, ...req.body};
+
+    if (userIndex > -1) {
+        users[userIndex] = updatedDetails;
+    } else {
+        return res.status(400).json({message: "user not found"});
+    }
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        if (err) console.log("not able to write the data");
+        return res.status(201).json({'User details updated sucessfully!': userId});
+    })
+
+})
+
+// Delete a user
+app.delete('/api/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    let deleteUser = users.filter((user) => user.id !== userId);
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(deleteUser), (err, data) => {
+        if (err) console.log("not able to write the data");
+        if (!err) {
+            return res.status(201).json({message: 'User deleted sucessfully!'});
+        } else {
+            return res.status(401).json({message: 'User Not Found!'})
+        }
+    })
 })
 
 app.listen(PORT, () => {
